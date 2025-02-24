f1291abd366cd000d0b3dcafb9e7c2e47c3d2c5d
Grigoriy <GrishaNaha@yandex.ru>
2025-02-24 16:50:50 +0300

    Удаление ненужных импортов

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,8 +6,7 @@ import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.time.LocalDate;
 
