e499435503f780f6df4c329fd671a51725041ef4
Grigoriy <GrishaNaha@yandex.ru>
2025-02-25 16:56:40 +0300

    Удаление аннотаций @Getter/@Setter в user

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,15 +2,11 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 
 import java.time.LocalDate;
 
 @Data
-@Getter
-@Setter
+
 public class User {
 
     private Long id;
